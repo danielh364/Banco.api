950425c3ed73f45f8dd47f2b27e1bb2a91462255
alumno <alumno@2A5PC04>
2015-10-30 09:44:37 +0100

    commit

---
+++ src/java/com/fpmislata/banco/presentation/security/FilterImplSecurity.java
@@ -0,0 +1,36 @@
+package com.fpmislata.banco.presentation.security;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class FilterImplSecurity implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+       String principal="permisos";
+       
+       if(principal.equals("permisos")){
+        System.out.println("Start Regular");
+           System.out.println("TRUE");
+       }
+       else{
+            filterChain.doFilter(servletRequest, servletResponse);
+            System.out.println("false");
+       }
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
